f64ebc5caf8058d090ae59bc3678c6b78e4e57ea
stormouse <stormouse@126.com>
2017-05-15 17:38:56 +0800

    half-completed prototype, need someone to help with UI..

---
+++ app.js
@@ -0,0 +1,192 @@
+
+/* ---  establish server  --- */
+var express = require('express')
+var app = express();
+var serv = require('http').Server(app);
+
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client', express.static(__dirname + '/client'));
+
+serv.listen(9999);
+console.log('Server started.')
+
+
+var io = require('socket.io')(serv);
+
+/* --- load story cards from db --- */
+// var ALL_STORY_CARDS = [];
+// const sqlite = require('sqlite3');
+// let db = new sqlite.Database('./database.db');
+// db.serialize(()=>{
+//     db.each("select * from storycards", (err, row) => {
+//         ALL_STORY_CARDS.push(row);
+//     });
+// });
+// db.close();
+
+
+/* --- import game module --- */
+
+const GameMod = require("./game");
+
+var User = function(id, socket, name){
+    var self = {
+        id : id,
+        socket : socket,
+        name : name,
+    };
+
+    return self;
+}
+User.list = {};
+
+
+var Player = function(id, socket){
+    var self = {
+        id : id,
+        socket : socket,
+    }
+
+    self.storyCards = [];
+    self.actionCards = [];
+    self.property = {
+        army : 10,
+        church : 10,
+        eco : 10,
+        people : 10,
+    };
+    return self;
+}
+
+
+
+
+/* --- create a game room to settle players --- */
+var Room = function(){
+
+    var self = {
+        sockets : {},
+        table : [-1, -1, -1, -1],
+        ready : [false, false, false, false],
+    };
+
+
+    self.Broadcast = function(event, data){
+        for(i in User.list){
+            User.list[i].socket.emit(event, data);
+        }
+    };
+
+
+    self.BroadcastSeatInfo = function(){
+        let playerNames = [
+            self.table[0] != -1 ? User.list[self.table[0]].name : "<空闲>",
+            self.table[1] != -1 ? User.list[self.table[1]].name : "<空闲>",
+            self.table[2] != -1 ? User.list[self.table[2]].name : "<空闲>",
+            self.table[3] != -1 ? User.list[self.table[3]].name : "<空闲>",
+        ];
+
+        let playerReady = [
+            self.ready[0] ? "[已准备]" : "",
+            self.ready[1] ? "[已准备]" : "",
+            self.ready[2] ? "[已准备]" : "",
+            self.ready[3] ? "[已准备]" : "",
+        ];
+
+        self.Broadcast("UpdateSeatInfo", {names:playerNames, ready:playerReady});
+    };
+
+
+    io.on('connection', function(socket){
+        socket.on("Login", function(data){
+            if(Object.keys(User.list) == 4){
+                socket.emit("LoginResponse", {reason : "房间已满"});
+                return;
+            }
+
+            if(data.paraphrase != "886") {
+                socket.emit("LoginResponse", {reason : "密码不正确"});
+                return;
+            }
+
+            for(i in User.list){
+                if(User.list[i].name == data.username){
+                    socket.emit("LoginResponse", {reason : "玩家昵称被占用"});
+                    return;
+                }
+            }
+
+            let uid = Math.floor(Math.random() * 100000);
+            while(uid in User.list){
+                uid = Math.floor(Math.random() * 100000);
+            }
+
+            socket.id = uid;
+
+            var user = User(uid, socket, data.username);
+            User.list[uid] = user;
+
+            socket.emit("LoginResponse", {id : uid, success:true});
+            self.Broadcast("AddToChat", "玩家["+user.name+"]加入了房间");
+            self.BroadcastSeatInfo();
+
+            socket.on("Sit", function(data){
+                let seatIndex = data.seat;
+                if(self.table[seatIndex] == -1)
+                {
+                    for(i in self.table){
+                        if(self.table[i] == socket.id) self.table[i] = -1;
+                    }
+                    self.table[seatIndex] = socket.id;
+                }
+                self.BroadcastSeatInfo();
+            });
+
+
+            socket.on("disconnect", function(){
+                for(i in self.table){
+                    if(self.table[i] == socket.id){
+                        self.table[i] = -1;
+                        self.ready[i] = false;
+                    }
+                }
+                self.Broadcast("AddToChat", "玩家["+User.list[socket.id].name+"]离开了房间");
+                self.BroadcastSeatInfo();
+                delete User.list[socket.id];
+            });
+
+
+            socket.on("Ready", function(){
+                for(i in self.table){
+                    if(self.table[i] == socket.id) self.ready[i] = true;
+                }
+
+                self.Broadcast("AddToChat", "玩家["+User.list[socket.id].name+"]已做好准备");
+                self.BroadcastSeatInfo();
+
+                let all_ready = true;
+                for(i in self.ready) all_ready = all_ready && self.ready[i];
+
+                if(all_ready){
+                    var players = {};
+                    for(id in User.list){
+                        let p = {
+                            id : id,
+                            socket : User.list[id].socket,
+                        };
+                        players[id] = p;
+                    }
+                    self.game = GameMod.Game(players, self.table, self.LoadStoryTypes);
+                }
+            });
+        });
+
+    });
+
+    return self;
+};
+
+var room = Room();
---
+++ game.js
@@ -0,0 +1,470 @@
+
+(function(){
+
+    const uuid = require('uuid/v1');
+
+    var StoryCard = function(id, prototype){
+        var self = {
+            id : id,
+            type : "story",
+            name : prototype.name,
+            story : prototype.story,
+            result : prototyle.result,
+        };
+
+        return self;
+    }
+
+
+    var ActionCard = function(id, prototype){
+        var self = {
+            id : id,
+            type : "action",
+            name : prototype.name,
+            action : prototype.action,
+        };
+
+        return self;
+    }
+
+    /* CONSTS OF STATE ENUM*/
+    const GameState = {
+        NotStarted : "NotStarted",
+        SetNewActivePlayer : "SetNewActionPlayer",
+        CallingForStoryCard : "CallingForStoryCard",
+        CallingForActionCard_1 : "CallingForActionCard_1",
+        CallingForActionCard_2 : "CallingForActionCard_2",
+        CallingForDecision : "CallingForDecision",
+        PerformAction : "PerformAction",
+        GameOver : "GameOver",
+    };
+
+
+    const GameEvent = {
+        // initialize
+        InitCardPile : "InitCardPile",
+
+        // gameplay
+        CallingForStoryCard : "CallingForStoryCard",
+        HandInStoryCard : "HandInStoryCard",
+        CallingForActionCard : "CallingForActionCard",
+        HandInActionCard : "HandInActionCard",
+        CallingForDecision : "CallingForDecision",
+        MakeDecision : "MakeDecision",
+
+
+        // state synchronization
+        SyncStoryCards : "SyncStoryCards",
+        SyncActionCards : "SyncActionCards",
+        UpdateGameInfo : "UpdateGameInfo",
+        AnnounceLoser : "AnnounceLoser",
+    }
+
+
+    var Game = function (players, table, story_types){
+        var self = {
+            state : GameState.NotStarted,
+            players : players,      // players[playerId] = playerInstance, the socket is in the instance.
+            table : table,          // table[seat] = playerId on that seat, seat = 0,1,2,3
+
+            storyTypes : story_types,
+            storyCards : {},
+            storyPile : [],         // card entities inside, not uid!
+            storyDiscarded : [],    // also card entities, not uid.
+
+            actionCards : {},
+            actionPile : [],
+            actionDiscarded : [],
+
+            
+            whosturn : Math.floor(Math.random() * 4),
+            whosactive : -1,
+            whostargeted : -1,
+            whosleft : [],
+
+            currentStory : null,
+            currentActions : [],
+            currentDecision : null,
+        };
+
+
+        self.PackInfo = function(){
+            var info = {
+                state : self.state,
+                whosturn : self.whosturn,
+                whosactive : self.whosactive,
+                whostargeted : self.whostargeted,
+                whosleft : self.whosleft,
+
+                properties : [
+                    self.players[self.table[0]].property,
+                    self.players[self.table[1]].property,
+                    self.players[self.table[2]].property,
+                    self.players[self.table[3]].property,
+                ],
+            };
+
+            if(self.currentStory != null)
+            {
+                info.currentStory = self.DesensitizeStoryCard(self.currentStory);
+            }
+
+            if(self.currentActions.length > 0)
+            {
+                info.currentActions = [];
+                for(i in self.currentActions){
+                    info.currentActions.push(self.currentActions[i].action);
+                }
+            }
+
+            if(self.currentDecision != null){
+                info.currentDecision = self.currentDecision;
+            }
+
+            return info; 
+        }
+
+        self.Broadcast = function(event, data){
+            for(i in self.players){
+                self.players[i].socket.emit(event, data);
+            }
+        }
+
+        self.shuffle = function(arr){
+            for(let i=arr.length-1; i>=0; i--){
+                let j = Math.floor(Math.random() * (i+1));
+                let t = actionPile[i]; actionPile[i] = actionPile[j]; actionPile[j] = t;
+            }
+        }
+
+
+        self.BuildStoryPile = function(){
+            for(let i = 0; i < 100; i++){
+                let storyPrototype = Math.floor(Math.random() * self.storyTypes.length);
+                let uid = uuid();
+                let storyCard = new StoryCard(uid, storyPrototype);
+                self.storyCards[uid] = storyCard;
+                self.storyPile.push(storyCard);
+            }
+            // randomly generated, no need to shuffle.
+        };
+
+
+        self.BuildActionPile = function(){
+            let n = 18;
+            for(let i = 0; i < n; i++){
+                let actionPrototype = i % self.actionTypes.length;
+                let uid = uuid();
+                let actionCard = new ActionCard(uid, actionPrototype);
+                self.actionPile.push(actionCard);
+            }
+
+            self.shuffle(self.actionPile);
+        };
+
+
+        /* Get next card in the pile, refill the pile if it is empty */
+        self.NextStoryCard = function(){
+            if(self.storyPile.length == 0){
+                self.storyPile = self.storyDiscarded.slice(); // copy the array
+                self.shuffle(self.storyPile);
+                self.storyDiscarded = [];
+            }
+            return self.storyPile.pop();
+        }
+
+        self.NextActionCard = function(){
+            if(self.actionPile.length == 0){
+                self.actionPile = self.actionDiscarded.slice();
+                self.shuffle(self.actionPile);
+                self.actionDiscarded = [];
+            }
+            return self.actionPile.pop();
+        }
+
+
+
+
+        /* Never let clients know the uid of the card, 
+           or they will cheat by modifying the game with browser */
+        self.DesensitizeStoryCard = function(storyCard){
+            return {
+                name : storyCard.name,
+                story : storyCard.story,
+                result : storyCard.result,
+            };
+        }
+
+        self.DesensitizeActionCard = function(actionCard){
+            return {
+                name : storyCard.name,
+                action : actionCard.action,
+            };
+        }
+
+
+
+        /* Deal the cards */
+        self.DealTheCards = function(){
+            for(let i = 0; i < 4; i++){
+                var clientStoryCards = [];
+                for(let j = 0; j < 5; j++){
+                    let c = NextStoryCard();
+                    players[table[i]].storyCards.push(c);
+                    clientStoryCards.push(self.DesensitizeStoryCard(c));
+                }
+
+                var clientActionCards = [];
+                for(let j = 0; j < 2; j++){
+                    let c = NextActionCard();
+                    players[table[i]].actionCards.push(c);
+                    clientActionCards.push(self.DesensitizeActionCard(c));    
+                }
+
+                players[table[i]].socket.emit(GameEvent.InitCardPile, {
+                    storyCards : clientStoryCards,
+                    actionCards : clientActionCards,
+                });
+            }
+        }
+
+
+
+        /* Game State Machine */
+        /* Transit In */
+        self.EnterState = function(newState){
+            self.state = newState;
+
+            switch(newState){
+                case GameState.SetNewActivePlayer:
+                {
+                    self.whosturn = (self.whosturn + 1) % 4;
+                    self.whosactive = self.whosturn;
+                    self.whostargeted = -1;
+                    self.whosleft = [];
+
+                    self.currentActions = [];
+                    self.currentStory = null;
+                    self.currentDecision = null;
+
+                    self.Transitor();
+                }
+                break;
+
+
+                case GameState.CallingForStoryCard:
+                {
+                    let player = self.players[table[whosturn]];
+                    player.socket.emit(GameEvent.CallingForStoryCard);
+                    player.socket.on(GameEvent.HandInStoryCard, self.Transitor);
+                    self.Broadcast(GameEvent.UpdateGameInfo, self.PackInfo());
+                }
+                break;
+
+
+
+                case GameState.CallingForActionCard_1:
+                {
+                    let player = self.players[table[whosactive]];
+                    player.socket.emit(GameEvent.CallingForActionCard);
+                    player.socket.on(GameEvent.HandInActionCard, self.Transitor);
+                    self.Broadcast(GameEvent.UpdateGameInfo, self.PackInfo());
+                }
+                break;
+
+                
+
+                case GameState.CallingForActionCard_2:
+                {
+                    let player = self.players[table[whosactive]];
+                    player.socket.emit(GameEvent.CallingForActionCard);
+                    player.socket.on(GameEvent.HandInActionCard, self.Transitor);
+                    self.Broadcast(GameEvent.UpdateGameInfo, self.PackInfo());
+                }
+                break;
+
+
+
+                case GameState.CallingForDecision:
+                {
+                    let player = self.players[table[whosactive]];
+                    player.socket.emit(GameEvent.CallingForDecision);
+                    player.socket.on(GameEvent.MakeDecision, self.Transitor);
+                    self.Broadcast(GameEvent.UpdateGameInfo, self.PackInfo());
+                }
+                break;
+
+
+                case GameState.PerformAction:
+                {
+                    let player = self.players[table[whostargeted]];
+                    let action = self.currentActions[self.decision].action;
+                    let value = self.currentStory.result[action];
+                    player.property.army += value.army;
+                    player.property.church += value.church;
+                    player.property.eco += value.eco;
+                    player.property.people += value.people;
+
+                    if(player.property.army <= 0 || player.property.army >= 20 ||
+                       player.property.church <= 0 || player.property.church >= 20 ||
+                       player.property.eco <= 0 || player.property.eco >= 20 ||
+                       player.property.people <= 0 || player.property.people >= 20)
+                       {
+                           self.EndGame({
+                                   reason : "kingdomfall",
+                                   loser : player
+                               });
+                       }
+                    else {
+                        self.Broadcast(GameEvent.UpdateGameInfo, self.PackInfo());
+                        self.Transitor();
+                    }
+                    
+                }
+                break;
+
+            }
+
+        }
+
+        /* Transit Out */
+        self.Transitor = function(data=null){
+            switch(self.state){
+                case GameState.SetNewActivePlayer:
+                self.EnterState(GameState.CallingForStoryCard);
+                break;
+
+
+                case GameState.CallingForStoryCard:
+                {
+                    // security check
+                    if(data.cardIndex >= 5) {self.EnterState(self.state); break;}
+
+                    let player = self.players[table[whosturn]];
+                    let card = player.storyCards[data.cardIndex];
+                    player.storyCards.splice(data.cardIndex, 1);
+                    player.storyCards.push(self.NextStoryCard());
+
+                    let clientStoryCards = [];
+                    for(i in player.StoryCards) 
+                        clientStoryCards.push(self.DesensitizeStoryCard(player.storyCards[i]));          
+                    player.socket.emit(GameEvent.SyncStoryCards, clientStoryCards);
+
+                    self.currentStory = card;
+                    self.storyDiscarded.push(card);
+                    self.whostargeted = data.target;
+                    for(i in table){
+                        if(i != self.whosturn && i != self.whostargeted)
+                            self.whosleft.push(i);
+                    }
+                    
+                    player.socket.removeEventListener(GameEvent.HandInStoryCard, self.Transitor);
+                    
+                    self.whosactive = self.whosleft[0];
+                    self.EnterState(GameState.CallingForActionCard_1);
+                }
+                break;
+
+
+                case GameState.CallingForActionCard_1:
+                {
+                    // security check
+                    if(data.cardIndex >= 2) {self.EnterState(self.state); break;}
+                    
+                    let player = self.players[table[whosactive]];
+                    let card = player.actionCards[data.cardIndex];
+                    player.actionCards.splice(data.cardIndex, 1);
+                    player.actionCards.push(self.NextActionCard());
+
+                    let clientActionCards = [];
+                    for(i in player.actionCards) 
+                        clientActionCards.push(self.DesensitizeActionCard(player.actionCards[i]));          
+                    player.socket.emit(GameEvent.SyncActionCards, clientActionCards);
+
+                    self.currentActions.push(card);
+                    self.actionDiscarded.push(card);
+                    player.socket.removeEventListener(GameEvent.HandInActionCard, self.Transitor);
+
+                    self.whosactive = self.whosleft[1];
+                    self.EnterState(GameState.CallingForActionCard_2);
+                }
+                break;
+
+
+                case GameState.CallingForActionCard_2:
+                {
+                    // security check
+                    if(data.cardIndex >= 2) {self.EnterState(self.state); break;}
+
+                    let player = self.players[table[whosactive]];
+                    let card = player.actionCards[data.cardIndex];
+                    player.actionCards.splice(data.cardIndex, 1);
+                    player.actionCards.push(self.NextActionCard());
+
+                    let clientActionCards = [];
+                    for(i in player.actionCards) 
+                        clientActionCards.push(self.DesensitizeActionCard(player.actionCards[i]));          
+                    player.socket.emit(GameEvent.SyncActionCards, clientActionCards);
+
+                    self.currentActions.push(card);
+                    self.actionDiscarded.push(card);
+                    player.socket.removeEventListener(GameEvent.HandInActionCard, self.Transitor);
+
+                    self.whosactive = self.whostargeted;
+                    self.EnterState(GameState.CallingForDecision);
+                }
+                break;
+
+                case GameState.CallingForDecision:
+                {
+                    // security check
+                    if(data.cardIndex >= 2) {self.EnterState(self.state); break;}
+
+                    self.currentDecision = data.decision;
+                    self.EnterState(GameState.PerformAction);
+                }
+                break;
+
+                case GameState.PerformAction:
+                self.EnterState(GameState.SetNewActivePlayer);
+                break;
+            }
+
+            
+        }
+
+
+        self.StartGame = function(){
+            self.BuildActionPile();
+            self.BuildStoryPile();
+            self.DealTheCards();
+            self.EnterState(GameState.SetNewActivePlayer);
+        }
+
+        self.EndGame = function(args){
+            switch(args.reason){
+                case "kingdomfall":
+                {
+                    for(i in table){
+                        if(table[i] == args.loser.id){
+                            Broadcast(GameEvent.AnnounceLoser, {loser : args.loser});
+                            break;
+                        }
+                    };
+                }
+                break;
+            }
+        }
+
+
+        return self;
+    }
+
+
+
+
+    module.exports.Game = function(players, table, story_types){
+        return Game(players, table, story_types);
+    }
+
+}());
