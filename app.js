c1a89d7f7239d1f4ccb13e89bd5ab37d355928db
stormouse <stormouse@126.com>
2017-05-19 09:48:08 +0800

    go

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 
 /* ---  establish server  --- */
 var express = require('express')
-var cors = require('cors')
 var app = express();
 var serv = require('http').Server(app);
 
-app.use(cors());
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
