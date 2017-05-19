35f2a7292ff249d9a47b35ba005fc2e2e4ad15b9
stormouse <stormouse@126.com>
2017-05-19 10:10:48 +0800

    add origin

--- app.js
+++ app.js
@@ -4,14 +4,6 @@ var express = require('express')
 var app = express();
 var serv = require('http').Server(app);
 
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1');
-    res.header("Content-Type", "application/json;charset=utf-8");
-    next();
-});
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
