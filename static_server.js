2d5989a3325df16c2acd248a92d7278279ceff7d
stormouse <stormouse@126.com>
2017-05-19 12:04:48 +0800

    update

--- static_server.js
+++ static_server.js
@@ -70,12 +70,21 @@ var server = http.createServer(function (req,res){
                     res.write(err.message);
                     res.end();
                 }else{
+                    // Website you wish to allow to connect
+                    res.setHeader('Access-Control-Allow-Origin', '*');
+
+                    // Request methods you wish to allow
+                    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+                    // Request headers you wish to allow
+                    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+                    // Set to true if you need the website to include cookies in the requests sent
+                    // to the API (e.g. in case you use sessions)
+                    res.setHeader('Access-Control-Allow-Credentials', true);
                     res.writeHead(200,
                         {
                             'Content-Type':resContentType, 
-                            'Access-Control-Allow-Origin': "*",
-                            'Access-Control-Allow-Headers': "Content-Type,Content-Length, Authorization, Accept,X-Requested-With",
-                            'Access-Control-Allow-Methods': "PUT,POST,GET,DELETE,OPTIONS",
                         });
                     res.write(data);
                     res.end();
