a690eae98e1dc92bcead82af255c087d154333c1
stormouse <stormouse@126.com>
2017-05-19 09:49:01 +0800

    no cors

--- app.js
+++ app.js
@@ -4,11 +4,6 @@ var express = require('express')
 var app = express();
 var serv = require('http').Server(app);
 
-app.use(function(req, res, next){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
